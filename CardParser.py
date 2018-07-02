4ab584077e317c2200f028c71a053862a7d60c5f
darthinvader <psaradakisJ@gmail.com>
2018-07-02 19:59:06 +0300

    Added Card Parser File and some comments

---
+++ CardParser.py
@@ -0,0 +1,4 @@
+# Each Card starts with ! ends with ! and has DimensionX DimensionY
+# Each Image starts with @ ends with @ and has Path , sizeX, sizeY, LocationX, LocationY
+# Each One line text starts with $ ends with $ and has text, sizeX , sizeY , Location X, LocationY
+# Each Multiline Text starts with % ends with % and has text, sizeX , sizeY , Location X, LocationY
